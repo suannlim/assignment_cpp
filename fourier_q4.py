0ffdec75f9be55e67a48fe23d569769263c3ddaf
suannlim <53931517+suannlim@users.noreply.github.com>
2020-11-11 18:23:56 +0000

    Update fourier_q4.py

--- fourier_q4.py
+++ fourier_q4.py
@@ -32,20 +32,43 @@ def g(t):
     return function
 
 
+"""
+In order to determine our sampling frequency, we have to ensure that it will
+lead to a Nyquist frequency that is larger than the max frequency of our
+function. As our input functions are not explicitly periodic, we assume that
+outside the time period, it repeats periodically, so we take T as the duration
+that our function is defined for
+
+As for the Gaussian, we have our range of values centered on 0 so N/2 values
+should be x<0 and N/2 values should be x>0 - so as to not shift our gaussian.
+As there is no specific frequency of the Gaussian function, we sample using an
+N that will give a frequency larger than that of h(t) but not too large so that
+it is too computationally taxing on the computer
+
+np.fft will automatically pad your sample value 
+
+how to find wmax????
+
+"""
+
+
+
+#Frequency of h(t)
+w_maxH=1/(7-5)
+
+
 #Number of sample points
 N=100
 
 #Sample spacing
-X=np.arange(-10,100,1/N)
-print(X)
+X=np.arange(-10,10,1/N)
+#print(X)
 
 #sample h function
 h_time=h(X)
 print(h_time)
-
 #sample g function
 g_time=g(X)
-
 #perform fourier transform
 
 h_fft=np.fft.fft(h_time)
@@ -54,9 +77,18 @@ g_fft=np.fft.fft(g_time)
 #find the new x values(Frequency)
 xf=(2*np.pi)/X
 
+#fourier h(t)
 plt.figure(1)
+plt.title("Fourier h(t)")
+plt.xlabel("Frequency space")
+#plt.xlim(-10,10)
 plt.plot(xf,h_fft)
+
+#fourier g(t)
 plt.figure(2)
+plt.title("Fourier g(t)")
+plt.xlabel("Frequency space")
+#plt.xlim(-0.5,0.5)
 plt.plot(xf,g_fft)
 
 #Find the fft convolution and the convolution
@@ -64,7 +96,20 @@ fftConvo=h_fft*g_fft
 convo=np.fft.ifft(fftConvo)
 
 plt.figure(3)
+plt.title("Convolution of g(t) and h(t)")
+plt.xlabel("Time space")
+#plt.xlim(-300,0)
 plt.plot(X,convo)
+
+
+plt.figure(4)
+plt.plot(X,g_time,label="g(t)")
+plt.plot(X,h_time,label="h(t)")
+plt.plot(X,convo,label="convolution")
+plt.legend()
+
+
+
 
 
 
